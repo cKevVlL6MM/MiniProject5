1484bdd64f143b920d7b432d44c7002efc0f53e2
pelletiern <pelletiern@eisti.fr>
2016-01-07 11:19:43 +0100

    Upload/Download sans style, à raccorder au reste, work in progress

---
+++ JOBBACKUP/WebContent/WEB-INF/views/FileDownload.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Telecharger Documents</title>
+</head>
+<body>
+<form method="get" action="documentDownload" ><input class="btn btn-primary" type="submit" value="Telecharger"/></form>
+<c:if test="${not empty nomsFichiers}">
+<c:forEach var="i" begin="0" end="${(fn:length(nomsFichiers))-1}" step="1">
+<h4><b><c:out value="${nomsFichiers.get(i)}"/></b></h4>
+<form method="post" action="documentDownload" ><input type="hidden" name="string" value="${nomsFichiers.get(i)}"/><input class="btn btn-primary" type="submit" value="Telecharger"/></form>
+</c:forEach>
+</c:if>
+</body>
+</html>
---
+++ JOBBACKUP/src/com/controllers/DocumentUploadController.java
@@ -0,0 +1,93 @@
+package com.controllers;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.persistence.Transient;
+import javax.servlet.http.HttpServletRequest;
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.jdbc.BinaryStream;
+import org.hibernate.type.SerializationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import hibernate.model.TableDocuments;
+
+@Controller
+@RequestMapping(value="/documentUpload")
+public class DocumentUploadController {
+	
+	private static Session session;
+
+	public static BigDecimal convertIntToBD(int integer)
+    {
+     return  new BigDecimal( (double)integer );
+    }
+	
+	@RequestMapping(method = RequestMethod.POST)
+	protected ModelAndView uploadDocument (@RequestParam("file") MultipartFile multiFile) throws IOException, SerialException, SQLException{
+		 if (!multiFile.isEmpty()) {
+			 		
+	            	SessionFactory sf =  new Configuration().configure("/hibernate.cfg.xml").buildSessionFactory();
+	            	Session sessions= sf.openSession();
+	            	
+	            	 byte[] bytes=multiFile.getBytes();
+	            	 
+	            	
+	            	/*
+	            	 Query query=sessions.createSQLQuery("insert into TABLE_DOCUMENTS(UPLOAD_ID, FILE_NAME, FILE_DATA) values (SQ_DOC.NEXTVAL,:FILENAME,:FILEDATA)");
+		        	query.setParameter("FILENAME",multiFile.getOriginalFilename());
+		        	query.setSerializable("FILEDATA", bytes);
+		        	query.executeUpdate();*/
+	            	 
+		        	Connection test;
+					try {
+						test = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","JOBEISTI","JOBEISTI");
+						PreparedStatement stmt=test.prepareStatement("insert into TABLE_DOCUMENTS(UPLOAD_ID, FILE_NAME, FILE_DATA) values (SQ_DOC.NEXTVAL,?,?)");
+						stmt.setString(1, multiFile.getOriginalFilename());
+						stmt.setBytes(2, bytes);
+						sessions.getTransaction().begin();
+						stmt.executeUpdate();
+						sessions.getTransaction().commit();
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+		        
+		        	
+	                System.out.println(multiFile.getOriginalFilename());
+				
+		 }
+	
+
+		
+		
+		return new ModelAndView("FileUpload");
+	}
+}
